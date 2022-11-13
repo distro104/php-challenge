50a4ce3b82a99855a174e871d581593233c4952b
distro104 <distro104@gmail.com>
2022-11-13 17:49:20 -0300

    update index.php(fixed)

--- challenge-01/index.php
+++ challenge-01/index.php
@@ -6,27 +6,45 @@ class Solution {
      */
     private $ops;
 
+    function array_clean($array_list)
+    {
+        $new_list = [];
+        foreach($array_list as $item)
+        {
+            if(!empty($item))
+            {
+                array_push($new_list,$item);
+            }
+        }
+        return $new_list;
+    }
+
     function calPoints($ops) {
         $array_list = [];
         $this->ops = $ops;
 
-        foreach ($ops as $item_ops)
+        foreach ($this->ops as $item_ops)
         {
             if(is_numeric($item_ops))
             {
                 array_push($array_list,$item_ops);
             }
-            elseif($item_ops == 'D')
+            elseif(($item_ops == 'D') && (count($array_list)>0))
             {
-                $array_list[count($array_list)-1] = end($array_list)*2;
+                array_push($array_list,end($array_list)*2);
             }
-            elseif($item_ops == '+')
+            elseif(($item_ops == 'C') && (count($array_list)>0))
             {
-                array_push($array_list, prev($array_list)+end($array_list));
+                $array_list = array_slice($array_list,0,-1);
             }
-            
-            return array_sum($array_list);
+            elseif(($item_ops == '+') && (count($array_list)>=2))
+            {
+                array_push($array_list, array_sum(array_slice($array_list,-2)) );
+            }
+            $array_list = $this->array_clean($array_list);
         }
+        print_r($array_list);
+        return array_sum($array_list);
     }
 }
 
