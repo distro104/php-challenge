92da7c8107306070b3a23206e7f5b10377cf89d3
distro104 <distro104@gmail.com>
2022-11-26 23:39:37 -0300

    design index complete | separate top/bot/content

---
+++ content-main.php
@@ -0,0 +1,25 @@
+<div class="menutop">
+        <ul>
+            <li><a href="#"></a></li>
+        </ul>
+    </div>
+    
+    <div class="content content-main">
+
+        <aside>
+            <p><a href="?page=index">Back...</a></p>
+            <h1>Challenge list</h1>
+            <ul>
+                <li><a href="#">DESCRIPTION LINK</a></li>
+                <li><a href="#">DESCRIPTION LINK</a></li>
+                <li><a href="#">DESCRIPTION LINK</a></li>
+                <li><a href="#">DESCRIPTION LINK</a></li>
+                <li><a href="#">DESCRIPTION LINK</a></li>
+                <li><a href="#">DESCRIPTION LINK</a></li>
+                <li><a href="#">DESCRIPTION LINK</a></li>
+            </ul>
+        </aside>
+
+        <article>asdf</article>
+    
+    </div>
