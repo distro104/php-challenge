474ed9eb1e67f36fc9fe72dc0b43f9f3f9367226
distro104 <distro104@gmail.com>
2022-11-27 12:32:47 -0300

    Update preview pics

--- content-main.php
+++ content-main.php
@@ -43,7 +43,7 @@
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M.2 10a11 11 0 0 1 19.6 0A11 11 0 0 1 .2 10zm9.8 4a4 4 0 1 0 0-8 4 4 0 0 0 0 8zm0-2a2 2 0 1 1 0-4 2 2 0 0 1 0 4z"/></svg>         
         </a>
             <h1 id="title">
-                Title of this challenge
+                Baseball Game
             </h1>
             <p id="description">
             Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
