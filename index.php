007209046fd4e89e5a43ce25314cbef92169e987
distro104 <distro104@gmail.com>
2022-11-29 15:52:07 -0300

    Fixed link index/main

--- index.php
+++ index.php
@@ -1,11 +1,14 @@
 <?php 
 
-$page = $_GET['page'];
+$data = new stdClass();
+$data->method = "post";
+$page = filter_input(INPUT_GET, "page", FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+
 $page = ($page == 'main') ? 'content-main.php' : 'content-index.php';
 
 include_once 'top.php';
 
-include_once $page ;
+include_once $page;
 
 include_once 'bottom.php' 
 
