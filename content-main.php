83ab3951399fede57bf12ba4bce01815a04fb8eb
distro104 <distro104@gmail.com>
2022-12-01 20:56:41 -0300

    layout main content finish.

--- content-main.php
+++ content-main.php
@@ -16,29 +16,27 @@
 
         <article>
             <a href="#">
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M.2 10a11 11 0 0 1 19.6 0A11 11 0 0 1 .2 10zm9.8 4a4 4 0 1 0 0-8 4 4 0 0 0 0 8zm0-2a2 2 0 1 1 0-4 2 2 0 0 1 0 4z"/></svg>         
-        </a>
-            <h1 id="title">
-                Baseball Game
-            </h1>
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M.2 10a11 11 0 0 1 19.6 0A11 11 0 0 1 .2 10zm9.8 4a4 4 0 1 0 0-8 4 4 0 0 0 0 8zm0-2a2 2 0 1 1 0-4 2 2 0 0 1 0 4z"/></svg>         
+            </a>
+            <h1>Baseball Game</h1>
             <p id="description">
-        You are keeping score for a baseball game with strange rules. The game consists of several rounds
-        where the scores of past rounds may affect future rounds' scores.
-        At the beginning of the game, you start with an empty record. You are given a list of strings ops,
-        where ops[i] is the ith operation you must apply to the record and is one of the following:
-        
-        An integer x - Record a new score of x.
-        '+' - Record a new score that is the sum of the previous two scores. It is guaranteed there will
-        always be two previous scores.
-        'D' - Record a new score that is double the previous score. It is guaranteed there will always be a
-         previous score.
-        'C' - Invalidate the previous score, removing it from the record. It is guaranteed there will
-         always, be a previous score.
-        
-        Return the sum of all the scores on the record.
+                You are keeping score for a baseball game with strange rules. The game consists of several rounds
+                where the scores of past rounds may affect future rounds' scores.
+                At the beginning of the game, you start with an empty record. You are given a list of strings ops,
+                where ops[i] is the ith operation you must apply to the record and is one of the following:
+                
+                An integer x - Record a new score of x.
+                '+' - Record a new score that is the sum of the previous two scores. It is guaranteed there will
+                always be two previous scores.
+                'D' - Record a new score that is double the previous score. It is guaranteed there will always be a
+                previous score.
+                'C' - Invalidate the previous score, removing it from the record. It is guaranteed there will
+                always, be a previous score.
+                
+                Return the sum of all the scores on the record.
             </p>
 
-            <h2>Example:</h2>
+            <h2>Example(s):</h2>
             <div id="exemple">
                 <div id="exemple_1">
                     <h3>Exemple 1:</h3>
